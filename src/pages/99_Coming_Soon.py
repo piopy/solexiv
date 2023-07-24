54efc8579e955d456bff3044b950b7f0083576ab
Antonio Pio Volgarino <burningham18@gmail.com>
2023-07-24 20:11:22 +0200

    Coming soon

--- src/pages/99_Coming_Soon.py
+++ src/pages/99_Coming_Soon.py
@@ -9,21 +9,28 @@ logo_and_page_title(st)
 def main():
     st.title("SoleXIV")
     st.markdown("By [piopy](https://github.com/piopy/)")
-    st.write("")
-    st.markdown(
-        '<img src="https://pngimg.com/uploads/github/github_PNG83.png" width=25%>',
-        unsafe_allow_html=True,
-    )
-    st.markdown("### [Visita il repository](https://github.com/piopy/solexiv)")
+    col1, col2 = st.columns([0.4, 0.6])
 
-    st.markdown(" ## Coming Soon")
+    with col1:
+        st.write("")
+        st.write("")
+        st.markdown(
+            '<img src="https://pngimg.com/uploads/github/github_PNG83.png" width=25%>',
+            unsafe_allow_html=True,
+        )
+        st.markdown("### [Visita il repository](https://github.com/piopy/solexiv)")
 
-    st.markdown(
+    with col2:
+        st.markdown(" ## Coming Soon")
+        st.markdown(
+            """
+        - [x] Total MongoDB Application
+        - [x] Export transazioni in Excel
+        - [ ] Previsione del bilancio futuro
+        - [ ] Notifiche per scadenze, pagamenti o obiettivi di risparmio
+        - [ ] Export mensile in PDF
         """
-    - [ ] Previsione del bilancio futuro
-    - [ ] Notifiche per scadenze, pagamenti o obiettivi di risparmio
-    """
-    )
+        )
 
 
 main()
